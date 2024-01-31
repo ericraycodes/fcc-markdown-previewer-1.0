27eace7504f70d8dd29df41afd122e765fc2cfbf
lasernoy <cloudy.nny07@gmail.com>
2024-01-31 14:30:40 +0800

    App 1.1 accessible structures

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,27 @@ function App() {
 
   return (
     <>
+    <header>Markdown Previewer</header>
+    <main>
     <div>
+      <div>
+        <label for='editor'>Editor</label>
+        <button className='resize-button' aria-label='resize button.'>+ / -</button>
+      </div>
       <textarea id='editor' defaultValue={'This is the textarea #editor'}></textarea>
-      <div id='preview'></div>
     </div>
+    <div>
+      <div>
+        <label for='preview'>Previewer</label>
+        <button className='resize-button' aria-label='resize button.'>+ / -</button>
+      </div>
+      <div id='preview'>
+        <p>
+          This is an arbitrary element holding the preview functionality of the app.
+        </p>
+      </div>
+    </div>
+    </main>
     </>
   )
 }
